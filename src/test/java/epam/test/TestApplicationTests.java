c717d0344babef96ec036b1b935d952e88ec979e
Mansur Muzaffarov <mansur.muzaffarov.b@gmail.com>
2023-02-18 17:28:24 +0100

    Initial commit

---
+++ src/test/java/epam/test/TestApplicationTests.java
@@ -0,0 +1,17 @@
+package epam.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads(ApplicationContext context) {
+        assertThat(context).isNotNull();
+    }
+
+}
